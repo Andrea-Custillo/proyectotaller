0b1348dbd880e35976e7e47d3a21146c2ec1560a
Andrea-Custillo <145853342+Andrea-Custillo@users.noreply.github.com>
2023-12-17 16:53:19 -0300

    first commit

---
+++ database/migrations/2023_12_16_230639_create_alumnos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',50);
+            $table->string('apellido',50);
+            $table->integer('edad');
+            $table->foreignId('id_grado')->constrained('grados')->onUpdate('cascade')->onDelete('restrict');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+};
